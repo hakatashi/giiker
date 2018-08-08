48d6dccadce5f174da1215f3d6dc656413348d4e
Koki Takahashi <hakatasiloving@gmail.com>
2018-08-09 02:58:54 +0900

    Update to support -2 move

--- index.babel.js
+++ index.babel.js
@@ -5,7 +5,12 @@ const SERVICE_UUID = '0000aadb-0000-1000-8000-00805f9b34fb';
 const CHARACTERISTIC_UUID = '0000aadc-0000-1000-8000-00805f9b34fb';
 
 const faces = ['B', 'D', 'L', 'U', 'R', 'F'];
-const turns = [1, 2, -1];
+const turns = {
+	0: 1,
+	1: 2,
+	2: -1,
+	8: -2,
+};
 
 class Giiker extends EventEmitter {
 	constructor() {
@@ -51,9 +56,11 @@ class Giiker extends EventEmitter {
 			notation = face;
 		} else if (amount === 2) {
 			notation = `${face}2`;
-		} else {
-			assert(amount === -1);
+		} else if (amount === -1) {
 			notation = `${face}'`;
+		} else {
+			assert(amount === -2);
+			notation = `${face}2'`;
 		}
 
 		this.emit('move', {face, amount, notation});
